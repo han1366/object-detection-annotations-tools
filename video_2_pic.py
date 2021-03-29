82cc95395143919f5846e8df25e064bb87e78e3b
han1366 <1366578286@qq.com>
2021-03-29 08:49:33 +0800

    将视频截成图片用于标注

---
+++ video_2_pic.py
@@ -0,0 +1,47 @@
+import cv2
+import os
+
+def video2image(num,getvideo,tofiles):
+
+    isExists=os.path.exists(tofiles)
+    if not isExists:
+        os.makedirs(tofiles)
+
+
+    print('开始运行:',end=' ')
+    vc = cv2.VideoCapture(getvideo)
+    c = 1
+
+    if vc.isOpened():
+        rval, frame = vc.read()
+    else:
+        rval = False
+
+    timeF = 150
+
+    while rval:
+        rval,frame = vc.read()
+        try:
+            if(c%timeF == 0):
+                cv2.imwrite(tofiles+'0325'+str(num)+'-'+str(c)+'.jpg',frame)
+                print(c//timeF)
+            c=c+1
+        except:
+            break
+        #cv2.waitKey(1)
+    vc.release()
+    print('\n 运行结束!')
+
+
+
+
+
+if __name__=='__main__':
+    # getvideo = 'J:/qc/01/ch01_20201206083023.mp4'
+    tofiles = './data/2021_0325_pic/'
+    file_dir = './data/forthVideo/'
+    for root, dirs, files in os.walk(file_dir):
+        for i, file in enumerate(files):
+            path = root + '/' +file
+            print(path)  # 当前路径下所有非目录子文件
+            video2image(i,path, tofiles)
